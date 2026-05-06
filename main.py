358cf149c1ad09a62fe7e49e366b33d4a35dbcac
justkinou <justkinou@proton.me>
2026-05-06 22:02:53 +0200

    add basic text detection

--- main.py
+++ main.py
@@ -7,13 +7,12 @@ app = FastAPI()
 
 @app.post("/detect-text")
 async def detect_text(files: list[UploadFile]):
-    pred_list = []
+    preds = ""
     for file in files:
         bytes_str = io.BytesIO(await file.read())
         try:
             image = Image.open(bytes_str)
         except:
             raise HTTPException(status_code=400, detail="Invalid image")
-        preds = pytesseract.image_to_string(image)
-        preds_list += [x for x in preds.split("\n")]
-    return {"results": preds_list }
+        preds += pytesseract.image_to_string(image)
+    return {"results": preds }
